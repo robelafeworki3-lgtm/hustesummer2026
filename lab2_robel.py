2beae0ce412623755f7ff829a8800f01e84de948
Robel Afeworki <robelafeworki@Robels-MacBook-Pro.local>
2026-06-25 13:48:46 -0700

    add lab2_robel,py

---
+++ lab2_robel.py
@@ -0,0 +1,19 @@
+fulL_name = "Robel Afeworki"
+nickname = "the goat"
+age = 67
+used_python_before = False
+hobbies = ["soccer", "mezmur", "novels"]
+favorites = {
+    "sports": ["soccer"],
+    "music": ["mezmur"],
+    "reading": ["novels"]
+}
+print(f"Full Name: {fulL_name}")
+print(f"Nickname: {nickname}")
+print(f"Age: {age}")
+print(f"Used Python Before: {used_python_before}")
+print(f"Hobbies: {hobbies}")
+print(f"Favorites: {favorites}")
+
+skills = "best soccer player in the world"
+language = "chinese
